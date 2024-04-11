8220cd5f5a383f3b3b2c37ecf3768e3425872d9b
Paul Muchui <paul.muchungi@gmail.com>
2024-04-11 12:20:54 +0300

    follows instructions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+
+
+
+
+
+function writeCards(arrayNames, eventMessage) {
+    let messages = []; 
+    for (let i = 0; i < arrayNames.length; i++) { 
+
+        let message = `Thank you, ${arrayNames[i]}, for the wonderful ${eventMessage} gift!`;
+        messages.push(message); 
+    }
+    return messages; 
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+countDown(10);
+
